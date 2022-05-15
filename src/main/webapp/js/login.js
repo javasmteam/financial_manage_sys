14c7a71ff061db49086f2b2c6eac2b3f782f7d27
YYONGYY <wds51021561@163.com>
2022-05-15 21:04:13 +0800

    更新

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,14 +1,14 @@
-
 function regUserF() {
-    userAct='';
-    userPwd='';
-    userName='';
-    userBirthday='';
-    userPhone='';
+    userAct = '';
+    userPwd = '';
+    userName = '';
+    userBirthday = '';
+    userPhone = '';
 }
+
 function userF() {
-    userAct= '';
-    userPwd= '';
+    userAct = '';
+    userPwd = '';
 }
 
 var app = new Vue({
@@ -17,49 +17,81 @@ var app = new Vue({
         //用户
         user: new userF(),
         //用户注册对象
-        reguser: new regUserF(),
+        regUser: new regUserF(),
         //显示注册弹框
         loginDialog: false,
         //项目绝对路径
         projectPath: "http://locathost:8088/financial_manage_sys",
         //登录验证
-        loginRules:{
-            userAct:[
-                {required:true,message:"请输入账号",trigger:'change'}
+        loginRules: {
+            userAct: [
+                {required: true, message: "请输入账号", trigger: 'change'}
             ],
-            userPwd:[
-                {required:true,message:"请输入密码",trigger:'change'}
+            userPwd: [
+                {required: true, message: "请输入密码", trigger: 'change'}
             ]
+        },
+        regRules: {
+            userAct: [
+                {required: true, message: "请输入账号", trigger: 'change'}
+            ],
+            userPwd: [
+                {required: true, message: "请输入密码", trigger: 'change'}
+            ],
+            userName: [
+                {required: true, message: "请输入姓名", trigger: 'change'}
+            ],
+            userBirthday: [
+                {required: true, message: "请输入生日", trigger: 'change'}
+            ],
+            userPhone: [
+                {required: true, message: "请输入良心方式", trigger: 'change'}
+            ],
         }
     },
     methods: {
         //注册请求
         reqReg: function () {
-            axios.post(this.projectPath + "?type=reqReg", this.reguser).then(resp => {
+            axios.post(this.projectPath + "?type=reqReg", this.regUser).then(resp => {
                 if (resp.data == "-1") {
                     this.$message.error("注册失败")
                 } else {
-                    this.$message({ message: "注册成功", type: "success" })
+                    this.$message({message: "注册成功", type: "success"})
                     this.reguser = new reguserF();
                     this.loginDialog = false;
                 }
             })
         },
+        //登录请求
         reqLogin: function () {
             axios.post(this.projectPath + "?type=reqLogin", this.user).then(resp => {
                 if (resp.data == "-1") {
                     this.$message.error("登录失败");
                 } else {
-                    this.$message({ message: "登录成功", type: "success" });
-                    location.href=this.projectPath+"/main.html"
+                    this.$message({message: "登录成功", type: "success"});
+                    location.href = this.projectPath + "/main.html"
                 }
             })
         },
-        userLogin: function () {
-            this.reqLogin;
+        //用户登录
+        userLogin(rules) {
+            this.$refs[rules].validate((valid) => {
+                if (valid) {
+                    this.reqLogin();
+                } else {
+                    return false;
+                }
+            })
         },
-        userReg: function () {
-            this.reqReg;
+        //用户注册
+        userReg: function (rules) {
+            this.$refs[rules].validate((valid) => {
+                if (valid) {
+                    this.reqReg()
+                } else {
+                    return false
+                }
+            })
         }
 
     },
