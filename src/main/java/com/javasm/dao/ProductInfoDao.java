a04bc9fd8b7e473022025a530e94ebc6bd549e18
mingmingmingaa <2448429318@qq.com>
2022-05-15 20:48:03 +0800

    产品信息dao，实现类增加

---
+++ src/main/java/com/javasm/dao/ProductInfoDao.java
@@ -0,0 +1,12 @@
+package com.javasm.dao;
+
+/**
+ * <h4>Financial_manage_sys</h4>
+ * <p>产品信息dao</p>
+ *
+ * @Author : zhao Yuanming
+ * @Date : 2022-05-15 20:41
+ * @Version : 1.0
+ **/
+public interface ProductInfoDao {
+}
---
+++ src/main/java/com/javasm/dao/ProductTypeDao.java
@@ -0,0 +1,13 @@
+package com.javasm.dao;
+
+/**
+ * <h4>Financial_manage_sys</h4>
+ * <p>产品系列dao</p>
+ *
+ * @Author : zhao Yuanming
+ * @Date : 2022-05-15 20:42
+ * @Version : 1.0
+ **/
+public interface ProductTypeDao {
+
+}
