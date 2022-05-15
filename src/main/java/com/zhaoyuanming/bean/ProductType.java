80838b54103daf6bc7d3e2a5ab6a7ffe5b1a37ed
mingmingmingaa <2448429318@qq.com>
2022-05-15 19:39:07 +0800

    change

---
+++ src/main/java/com/zhaoyuanming/bean/ProductType.java
@@ -0,0 +1,27 @@
+package com.zhaoyuanming.bean;
+
+import lombok.*;
+
+/**
+ * <h4>Financial_manage_sys</h4>
+ * <p>产品类型</p>
+ *
+ * @Author : zhao Yuanming
+ * @Date : 2022-05-15 19:16
+ * @Version : 1.0
+ **/
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ProductType {
+    private Integer productTypeId;
+    private Integer remitId;
+    private Integer productParentId;
+    private String productChannel;
+    private String productTypeChName;
+    private String productTypeEngName;
+    private Integer productTypeLv;
+    private Integer productTypeState;
+}
