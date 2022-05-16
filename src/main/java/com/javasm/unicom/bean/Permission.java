db5d9625184391cf933551211cc7ea4ed9c35d8f
YYONGYY <wds51021561@163.com>
2022-05-16 14:38:09 +0800

    更新

--- src/main/java/com/javasm/unicom/bean/Permission.java
+++ src/main/java/com/javasm/unicom/bean/Permission.java
@@ -5,11 +5,22 @@ package com.javasm.unicom.bean;
  * @date: 2022/5/15 23:39
  * @description:
  */
+
+/**
+ * permissionId:    权限id
+ * permissionCode:  权限代码
+ * permissionName:  权限名
+ * permissionPath:  权限页面路径
+ * permissionImage: 权限图标路径
+ * parentPermission:    上级权限id
+ * permissionState: 权限状态    0:删除 1:生效
+ */
 public class Permission {
     private int permissionId;
     private String permissionCode;
     private String permissionName;
     private String permissionPath;
     private String permissionImage;
+    private int parentPermission;
     private int permissionState;
 }
