ff92fd55070b7c21dbdcd6b12bb27299ac8158cd
YYONGYY <wds51021561@163.com>
2022-05-18 10:09:51 +0800

    更新

---
+++ src/main/webapp/js/userInfo.js
@@ -0,0 +1,21 @@
+var app = new Vue({
+    el:"#app",
+    data:{
+        //显示修改弹窗
+        setUserFlag:false,
+        //修改用户数据
+        setUser:{},
+        //显示用户信息对象
+        userInfo:{
+
+        },
+
+
+    },
+    methods:{
+
+    },
+    created:function (){
+
+    }
+})
